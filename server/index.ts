19fb37ae38eb5cf01d19bf3c5b34508c0f0cef17
Sachin Raja <sachinraja2349@gmail.com>
2022-08-27 09:06:09 -0700

    add package metadata

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,4 @@
-export type { AppRouter } from './router/_app.js'
+export type { AppRouter as PokemonRouter } from './router/_app.js'
 export type { Ability, Pokemon } from './types.js'
+
+export const trpcPokemonUrl = 'https://trpc-pokemon.sachinraja.workers.dev'
